88c0ffe6705e6d09dd1d82df045ea27671c69797
Yibrika <108902017+Yibrika@users.noreply.github.com>
2022-07-25 10:01:45 -0500

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+document.getElementById("btn").addEventListener("click", () => {
+  let creditCardNumber = document.getElementById("cardNumber").value;
+  let nameCard = document.getElementById("namecard").value;
+  //let surnameCard = document.getElementById("surnamecard").value;
+  if (creditCardNumber == " ") {
+    alert("DEBES INTRODUCIR UN NUMERO");
+    return document.getElementById("cardNumber").value;
+  }
+  if (nameCard == "") {
+    alert("DEBES INTRODUCIR TU NOMBRE Y APELLIDO");
+    return document.getElementById("namecard").value;
+  }
+  validarTarjeta(creditCardNumber);
+  maskyCard(creditCardNumber);
+  nombreComprador(nameCard);
+});
+
+function validarTarjeta(creditCardNumber) {
+  let exitoso = validator.isValid(creditCardNumber);
+  if (exitoso === true) {
+    console.log("tarjeta valida");
+    document.getElementById("mensaje-valido").innerHTML = "su numero de tarjeta es valido ";
+  } else {
+    console.log(
+      "Su tarjeta es invalida por favor verifiquela e intente nuevamente"
+    );
+    document.getElementById("mensaje-invalido").innerHTML = "Su tarjeta es invalida por favor verifiquela e intente nuevamente";
+  }
+}
+function maskyCard(creditCardNumber) {
+  //document.getElementById("numberMaskify").innerHTML =
+  document.getElementById("cardNumber").value = validator.maskify(creditCardNumber);
+}
+
+function nombreComprador(nameCard) {
+  document.getElementById("nameResul").innerHTML = nameCard;
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
 const validator = {
-  // ...
-};
-
-export default validator;
+  isValid: (creditCardNumber) => {
+    let sumNumber = 0;
+    let totalSum = 0;
+    //Convirtiendo el string en array y reversando
+    creditCardNumber = creditCardNumber.split("").reverse();
+    //vamos a iterar en las posiciones para poder hacer la multiplicacion
+    for (var i = 0; i < creditCardNumber.length; i++) {
+      //buscamos las posiciones pares
+      if (i % 2 === 1) {
+        //multiplicamos el valor de la posicion par por 2
+        creditCardNumber[i] = creditCardNumber[i] * 2;
+        //Si es mayor a 10
+        if (creditCardNumber[i] >= 10) {
+          //Si es mayor a 10 debemos convertir el numero en string y de string a array
+          sumNumber = creditCardNumber[i].toString().split("");
+          //console.log("El valor" + sumNumber);
+          //una vez convertido en array y teniendo la posicion 0 y 1 las sumamos
+          creditCardNumber[i] = parseInt(sumNumber[0]) + parseInt(sumNumber[1]);
+        }
+      }
+              //multiplicamos el valor de la posicion par por 2
+      //Una vez sumados, tenemos una variable acumuladora que contiene los numeros restantes de nuestro input
+            totalSum = totalSum + parseInt(creditCardNumber[i]);
+          }
+          //Pasamos aplicar nuestro modulo 10
+          let validCard = totalSum % 10 == 0 ? true : false;
+          return validCard;
+        },
+        maskify: (creditCardNumber) => {
+          let enmascarada = creditCardNumber.length;
+          let masky = "";
+          for (let i = 0; i < enmascarada - 4; i++) {
+            masky += "#";
+          }
+          return masky + creditCardNumber.substring(enmascarada - 4, enmascarada);
+        },
+      };
+      
+      export default validator;
+      
